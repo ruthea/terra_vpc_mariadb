5cf94eeac13bef9dadb73e6308a05ff892713531
Austin Rutherford <austinrutherford@gmail.com>
2019-02-02 13:10:08 -0600

    Update userdata_loadtest.sh

--- userdata_loadtest.sh
+++ userdata_loadtest.sh
@@ -26,7 +26,7 @@ case $instancetype in
 esac
 
 yum install -y wget
-wget -P /tmp https://downloads.sourceforge.net/project/hammerdb/HammerDB/HammerDB-3.1/HammerDB-3.1-Linux-x86-64-Install?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fhammerdb%2Ffiles%2FHammerDB%2FHammerDB-3.1%2FHammerDB-3.1-Linux-x86-64-Install%2Fdownload&ts=1549133620
+wget -P /tmp -O HammerDB-3.1-Linux-x86-64-Install https://downloads.sourceforge.net/project/hammerdb/HammerDB/HammerDB-3.1/HammerDB-3.1-Linux-x86-64-Install?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fhammerdb%2Ffiles%2FHammerDB%2FHammerDB-3.1%2FHammerDB-3.1-Linux-x86-64-Install%2Fdownload&ts=1549133620
 chmod +x /tmp/HammerDB-3.1-Linux-x86-64-Install
 ./tmp/HammerDB-3.1-Linux-x86-64-Install --mode silent
 
