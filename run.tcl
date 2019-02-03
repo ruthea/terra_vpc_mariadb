f41646f870b4bd374c854858f9cc47b71e55e801
Austin Rutherford <austinrutherford@gmail.com>
2019-02-03 08:05:35 -0600

    Update run.tcl

--- run.tcl
+++ run.tcl
@@ -4,7 +4,7 @@ dbset db mysql
 diset connection mysql_host 10.0.2.20
 diset connection mysql_port 3306
 diset tpcc mysql_user dba
-diset tpcc mysql_password demo_password
+diset tpcc mysql_pass demo_password
 diset tpcc mysql_count_ware 800
 diset tpcc mysql_partition true
 diset tpcc mysql_num_vu 64
