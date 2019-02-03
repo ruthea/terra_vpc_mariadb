9afde5942f7381144732ebb8325ebcd20c5b3b1e
Austin Rutherford <austinrutherford@gmail.com>
2019-02-03 08:30:21 -0600

    Update userdata_loadtest.sh

--- userdata_loadtest.sh
+++ userdata_loadtest.sh
@@ -29,7 +29,10 @@ yum install -y wget
 wget -P /tmp https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.25-1.el7.x86_64.rpm-bundle.tar
 tar -xvf /tmp/mysql-5.7.25-1.el7.x86_64.rpm-bundle.tar -C /tmp
 wget -O /tmp/HammerDB-3.1-Linux-x86-64-Install https://downloads.sourceforge.net/project/hammerdb/HammerDB/HammerDB-3.1/HammerDB-3.1-Linux-x86-64-Install?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fhammerdb%2Ffiles%2FHammerDB%2FHammerDB-3.1%2FHammerDB-3.1-Linux-x86-64-Install%2Fdownload&ts=1549133620
+sleep 5
+touch /tmp/abouttochmod.touch
 chmod +x /tmp/HammerDB-3.1-Linux-x86-64-Install
+touch /tmp/shouldbedonewithchmod.touch
 yum install -y /tmp/*.rpm
 /tmp/HammerDB-3.1-Linux-x86-64-Install --mode silent
 
