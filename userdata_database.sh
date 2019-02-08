3c58852989232cf3995f6781c98fb4690806da2b
Austin Rutherford <austinrutherford@gmail.com>
2019-02-08 00:06:40 -0600

    Update userdata_database.sh

--- userdata_database.sh
+++ userdata_database.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -v
 instancetype=$(curl http://169.254.169.254/latest/meta-data/instance-type)
 echo "instance type is: " $instancetype
+
 case $instancetype in
  "i3.large" | "i3.xlarge" | "i3.2xlarge" )
    mkdir -p /var/lib/mysql
@@ -10,10 +11,20 @@ case $instancetype in
    mount -a
    ;;
 
- "i3.4xlarge" | "i3.8xlarge" | "i3.16xlarge" )
+ "i3.4xlarge" )
    mkdir -p /var/lib/mysql
    yum -y install mdadm
    mdadm --create --verbose /dev/md0 --level=stripe --raid-devices=2 /dev/nvme0n1 /dev/nvme1n1
+   mkfs.ext4 -E nodiscard /dev/md0
+   mount -o discard /dev/md0 /var/lib/mysql
+   echo "/dev/md0 /var/lib/mysql ext4 defaults,nofail,discard 0 2" >> /etc/fstab
+   mount -a
+   ;;
+
+ "i3.8xlarge" | "i3.16xlarge" )
+   mkdir -p /var/lib/mysql
+   yum -y install mdadm
+   mdadm --create --verbose /dev/md0 --level=stripe --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
    mkfs.ext4 -E nodiscard /dev/md0
    mount -o discard /dev/md0 /var/lib/mysql
    echo "/dev/md0 /var/lib/mysql ext4 defaults,nofail,discard 0 2" >> /etc/fstab
